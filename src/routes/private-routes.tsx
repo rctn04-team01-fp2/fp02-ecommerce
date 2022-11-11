47af45cf6bc3636c3dc0798ee8c1eafe6d95e44b
tatasulysta <tatasulystaaa@gmail.com>
2022-11-11 11:37:38 +0700

    fix: private route auth

--- src/routes/private-routes.tsx
+++ src/routes/private-routes.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/nav-bar';
+import { getToken } from '../helpers';
 import { NotFound } from '../pages/not-found';
-import { userData } from '../utils/account-data';
 
 export const PrivateRoutes = () => {
-  const token = localStorage.getItem('login-token');
-  const isAuth = userData.filter((user) => user.token === token);
+  const token = getToken();
+
   //eslint-disable-next-line
-  return isAuth.length ? (
+  return token ? (
     <>
       <Navbar auth={true} />
       <Outlet />
