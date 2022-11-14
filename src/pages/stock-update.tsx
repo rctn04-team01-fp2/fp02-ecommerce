8fbaa0d33d24592f43232d6dd2b11f749a2f116a
tatasulysta <tatasulystaaa@gmail.com>
2022-11-14 20:29:37 +0700

    feat: table head component

--- src/pages/stock-update.tsx
+++ src/pages/stock-update.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import StockItem from '../components/stock-item';
+import { TableHead } from '../components/table-head';
 import { selectProducts } from '../features/product/product-slice';
+import { stockTableHead } from '../utils/table-head';
 
 export default function StockUpdate() {
   const { products } = useSelector(selectProducts);
@@ -11,25 +13,7 @@ export default function StockUpdate() {
       style={{ width: 'fit-content', minWidth: '60%' }}
     >
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 ">
-          <tr>
-            <th scope="col" className="py-3 px-6">
-              Image
-            </th>
-            <th scope="col" className="py-3 px-6">
-              Product Information
-            </th>
-            <th scope="col" className="py-3 px-6">
-              Category
-            </th>
-            <th scope="col" className="py-3 px-6">
-              Stock
-            </th>
-            <th scope="col" className="py-3 px-6">
-              Price
-            </th>
-          </tr>
-        </thead>
+        <TableHead data={stockTableHead} />
         <tbody>
           {products.map((product, index) => (
             <StockItem
