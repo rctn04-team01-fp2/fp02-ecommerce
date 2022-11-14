a92c19035c92d87c430c605420803edab749f2e9
tatasulysta <tatasulystaaa@gmail.com>
2022-11-14 12:58:23 +0700

    add restore state after logout

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { logout } from '../features/user/user-slice';
 import { NavItem } from '../utils/nav-item';
 interface Props {
   auth: boolean;
@@ -7,6 +9,7 @@ interface Props {
 
 export default function Navbar(props: Props) {
   const nav = props.auth ? NavItem.true : NavItem.false;
+  const dispatch = useDispatch();
   return (
     <div className="bg-purple h-2/3 py-8 px-16 min-w-full min-h-1 justify-between flex text-baseWhite font-bold">
       <img src="/brand-logo.svg" alt="logo" className="h-32 py-4 px-32" />
@@ -17,7 +20,10 @@ export default function Navbar(props: Props) {
             <NavLink
               to={item.link}
               key={item.id}
-              onClick={() => localStorage.removeItem('login-token')}
+              onClick={() => {
+                localStorage.removeItem('login-token');
+                dispatch(logout());
+              }}
             >
               {item.name}
             </NavLink>
