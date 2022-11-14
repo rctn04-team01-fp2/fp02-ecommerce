418fe83c41d15bfb99d07fd6caa893feae72cbd1
tatasulysta <tatasulystaaa@gmail.com>
2022-11-14 19:53:47 +0700

    feat: admin type navbar

--- src/utils/nav-item.ts
+++ src/utils/nav-item.ts
@@ -11,21 +11,40 @@ export const NavItem = {
       link: '/login',
     },
   ],
-  true: [
-    {
-      id: '3',
-      name: 'Products',
-      link: '/products',
-    },
-    {
-      id: '4',
-      name: 'Cart',
-      link: '/cart',
-    },
-    {
-      id: '5',
-      name: 'Logout',
-      link: '/',
-    },
-  ],
+  true: {
+    admin: [
+      {
+        id: '3',
+        name: 'Stock update',
+        link: '/stock-update',
+      },
+      {
+        id: '4',
+        name: 'Rekap Penjualan',
+        link: '/rekap-penjualan',
+      },
+      {
+        id: '5',
+        name: 'Logout',
+        link: '/',
+      },
+    ],
+    user: [
+      {
+        id: '3',
+        name: 'Products',
+        link: '/products',
+      },
+      {
+        id: '4',
+        name: 'Cart',
+        link: '/cart',
+      },
+      {
+        id: '5',
+        name: 'Logout',
+        link: '/',
+      },
+    ],
+  },
 };
