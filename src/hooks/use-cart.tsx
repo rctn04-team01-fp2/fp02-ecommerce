19506f40a19af60e27f13fb8f542717d53e01840
ferdy <ferdy.lim@springkraf.com>
2022-11-15 11:53:17 +0700

    fix: use-cart

--- src/hooks/use-cart.tsx
+++ src/hooks/use-cart.tsx
@@ -4,10 +4,13 @@ import * as React from 'react';
 
 export function useCart(props: { username: string }) {
   const { carts: _carts } = useSelector(selectCarts);
-  const carts = React.useMemo(
-    () =>
-      _carts.find((cart) => cart.username === props.username)?.products ?? [],
-    [_carts],
-  );
+  const carts = React.useMemo(() => {
+    const _cart = _carts.find((cart) => cart.username);
+    if (_cart) {
+      return _cart.products;
+    } else {
+      return [];
+    }
+  }, [_carts]);
   return { carts };
 }
